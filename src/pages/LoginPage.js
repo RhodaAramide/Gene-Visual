a62724288e2b4f5229488b41962af34ef5a35b5e
RhodaAramide <olabisiabolarin5@gmail.com>
2024-04-23 08:13:57 +0100

    Added sign up and login page

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -2,7 +2,30 @@ import React from 'react'
 
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
+    <div>
+      
+        {/* <div class="imgcontainer">
+          <img src="img_avatar2.png" alt="Avatar" class="avatar">
+        </div> */}
+
+        <div class="container">
+          <label for="username"><b>Username</b></label>
+          <input type="text" placeholder="Enter Username" className="username" required />
+
+          <label for="password"><b>Password</b></label>
+          <input type="password" placeholder="Enter Password" className="password" required />
+
+          <button type="submit">Login</button>
+          {/* <label>
+            <input type="checkbox" checked="checked" name="remember"> Remember me
+          </label> */}
+        </div>
+
+        <div class="container">
+          <button type="button" class="cancelbtn">Cancel</button>
+          <span class="password">Forgot <a href="#">password?</a></span>
+        </div>      
+    </div>
   )
 }
 
--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -2,7 +2,20 @@ import React from 'react'
 
 const SignUpPage = () => {
   return (
-    <div>SignUpPage</div>
+    <div>
+            
+        <div class="container">
+          <label for="username"><b>Username</b></label>
+          <input type="text" placeholder="Enter Username" className="username" required />
+
+          <label for="password"><b>Password</b></label>
+          <input type="password" placeholder="Enter Password" className="password" required />
+
+          <button type="submit">Register</button>
+          
+        </div>
+             
+    </div>
   )
 }
 
